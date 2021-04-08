f8f19ad386593a0465cae51d86a2d9e28be270a6
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-04-08 14:05:43 +1000

    Successful async fetch

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {Component} from 'react';
+import './App.css';
+
+
+export default class App extends Component {
+state = {
+    peopleInSpace: []
+}
+
+render() {
+    return (
+        <ul>
+            {this.state.peopleInSpace.map(person => <li>{person.name}0</li>)}
+        </ul>
+    )
+}
+
+componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(data => {
+        this.setState({
+            peopleInSpace: data.people
+        })
+    })
+}
+
+
+}
